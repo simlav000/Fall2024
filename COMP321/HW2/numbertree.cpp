84cafbb89d193e21d21ca210ff389bd9e8ac4da0
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-09-12 16:37:53 -0400

    Solved

--- COMP321/HW2/numbertree.cpp
+++ COMP321/HW2/numbertree.cpp
@@ -1,45 +1,33 @@
 #include <cmath>
 #include <iostream>
 
-struct Node {
-    int aData;
-    Node* left;
-    Node* right;
-
-    Node(int root) :
-        aData(root), left(nullptr), right(nullptr) {}
-};
-
-Node* createPerfectTree(int height, int& count) {
-    if (height < 0) return nullptr;
-
-    Node* root = new Node(count--);
-    root->left = createPerfectTree(height - 1, count);
-    root->right = createPerfectTree(height - 1, count);
-
-    return root;
-}
-
-void printAnswer(Node pTree, std::string instructions) {
-    if (instructions.empty()) {
-        std::cout << pTree.aData << std::endl;
-    }
+int traverse(std::string instructions, int diff) {
+    if (instructions.empty()) return diff;
 
     else if (instructions[0] == 'L') {
-        printAnswer(pTree->left, instructions.erase(0,1));
+        return traverse(instructions.erase(0, 1), 2*diff);
     }
 
-    printAnswer(pTree->right, instructions.erase(0,1));
+    else {
+        return traverse(instructions.erase(0,1), 2*diff + 1);
+    }
 }
 
 int main() {
     int height;
     std::string instructions;
 
-    std::cin >> height >> instructions;
+    std::cin >> height;
 
-    int root =  pow(2, height) - 1;
-    int& count = root;
+    int root =  pow(2, height+1);
+    int diff = 1;
 
-    Node* pTree = createPerfectTree(height, count);
+    if (std::cin >> instructions) {
+        std::cout << root - traverse(instructions, diff) << std::endl;
+    }
+
+    else {
+        std::cout << root - 1 << std::endl;
+    }
+
 }
