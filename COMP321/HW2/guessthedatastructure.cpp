0f6184d0b59efd1a57a1af681a7cf976ba2705f2
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-09-12 20:50:34 -0400

    Solved and proud of it

--- COMP321/HW2/guessthedatastructure.cpp
+++ COMP321/HW2/guessthedatastructure.cpp
@@ -1,24 +1,51 @@
+#include <iostream>
 #include <functional>
 #include <queue>
 #include <stack>
-#include <stdlib.h>
-#include <iostream>
 
 int main() {
-    std::stack<int> stack;
-    std::queue<int> queue;
-    std::priority_queue<int, std::vector<int>, std::greater<int>> lpq;
-    std::priority_queue<int, std::vector<int>, std::less<int>> gpq;
-
     int n;
 
     while (std::cin >> n) {
-        for (int i = 0; i < n; i++) {
-            int op, num;
-            std::cin >> op >> num;
-            std::cout << op << num << std::endl;
+        int cmd, inp;
+
+        std::queue<int> q;
+        std::stack<int> s;
+        std::priority_queue<int, std::vector<int>, std::less<int>> pq;
+
+        bool is_q = true;
+        bool is_s = true;
+        bool is_pq = true;
+
+        while (n--) {
+            std::cin >> cmd >> inp;
+
+            if (cmd == 1) {
+                q.push(inp);
+                s.push(inp);
+                pq.push(inp);
+            }
+
+            if (cmd == 2) {
+                if (q.empty() || s.empty() || pq.empty()) {
+                    is_q = false; is_s = false; is_pq = false;
+                }
+                else {
+                    int q_front = q.front(); q.pop();
+                    int s_top = s.top(); s.pop();
+                    int pq_top = pq.top(); pq.pop();
+
+                    if (inp != q_front) is_q = false;
+                    if (inp != s_top) is_s = false;
+                    if (inp != pq_top) is_pq = false;
+                }
+            }
         }
+
+        if (!is_q && !is_s && !is_pq) std::cout << "impossible" << std::endl;
+        else if (is_q && is_s || is_s && is_pq || is_q && is_pq ) std::cout << "not sure" << std::endl;
+        else if (is_q)  std::cout << "queue" << std::endl;
+        else if (is_s)  std::cout << "stack" << std::endl;
+        else if (is_pq) std::cout << "priority queue" << std::endl;
     }
-
-
 }
