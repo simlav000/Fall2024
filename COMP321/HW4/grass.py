7ef03f89b91c4e89b32f12d99cef580ccc8c6229
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-10-05 21:18:55 -0400

    Updates on competitive programming questions

---
+++ COMP321/HW4/grass.py
@@ -0,0 +1,11 @@
+import sys
+
+n, l, w = map(int, input().split())
+
+sprinklers = []
+
+for _ in range(n):
+    sprinklers.append(list(map(int, input().split())))
+
+for s in sprinklers:
+    dx = 
