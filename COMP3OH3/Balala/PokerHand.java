c0bcb5fa752784aa05d55cde4a76a7bc46a7e740
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-10-09 00:33:33 -0400

    Added base chips and mult for poker hands and ways to get and set em

--- COMP3OH3/Balala/PokerHand.java
+++ COMP3OH3/Balala/PokerHand.java
@@ -2,10 +2,46 @@ import java.util.Map;
 import java.util.HashMap;
 
 public enum PokerHand {
-    FIVEOFAKIND, STRAIGHTFLUSH, FOUROFAKIND, FULLHOUSE, FLUSH, STRAIGHT,
-    THREEOFAKIND, TWOPAIR, ONEPAIR, HIGHCARD;
+    FIVEOFAKIND(120, 12), FLUSHHOUSE(140, 14), FLUSHFIVE(160, 16),
+    STRAIGHTFLUSH(100, 8), FOUROFAKIND(60, 7), ROYALFLUSH(100, 8),
+    FULLHOUSE(40, 4), FLUSH(35, 4), STRAIGHT(30, 4), THREEOFAKIND(30, 3),
+    TWOPAIR(20, 2), PAIR(10, 2), HIGHCARD(5, 1);
 
-    public PokerHand categorize(Hand pHand) {
+    private int aChips;
+    private int aMult;
+    private int aNumTimesPlayed = 0;
+
+    PokerHand(int pChips, int pMult) {
+        aChips = pChips;
+        aMult = pMult;
+    }
+
+    public int playPokerHand() {
+        aNumTimesPlayed++;
+        return (int) (aChips * aMult); // Adjust score by multiplier
+    }
+
+    public int getTimesPlayed() {
+        return aNumTimesPlayed;
+    }
+
+    public void improveChips(int pChips) {
+        aChips += pChips;
+    }
+
+    public void improveMult(double pMult) {
+        aMult += pMult;
+    }
+
+    public int getChips() {
+        return aChips;
+    }
+
+    public double getMult() {
+        return aMult;
+    }
+
+    public PokerHand getPokerHand(Hand pHand) {
         HashMap<Rank, Integer> rankCount = new HashMap<>();
         for (Card c : pHand) {
             Rank rank = c.rank();
@@ -28,8 +64,13 @@ public enum PokerHand {
                 return THREEOFAKIND;
             }
 
+            else if (count == 2) {
+                return PAIR;
+            }
+
         }
 
         return HIGHCARD;
     }
+
 }
