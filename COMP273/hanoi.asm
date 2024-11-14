e1830b28ffc12634a8f7b1f962d4a53ff467518b
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-11-13 23:31:14 -0500

    Integrated printing helper and first recursive call of hanoi. Well not really. It isn't recursing at all.

--- COMP273/hanoi.asm
+++ COMP273/hanoi.asm
@@ -2,35 +2,118 @@
 # Student ID: 261 051 325
 # Name: Simon Lavoie
 
-.data
-AlgorithmType:	.word 0		# TODO: Which Algorithm to run!
-				# 0 Recursive
-				# 1 Non-recursive
-	
-# TODO: add any variables here you if you need
-
-# TODO END
-.text
+    .data
+Step:          .asciiz "Step "
+Colon:         .asciiz ": "
+Move:         .asciiz "move disk "
+From:          .asciiz " from "
+To:            .asciiz " to "
+Newline:       .asciiz "\n"
+AlgorithmType: .word 0
+		# 0 Recursive
+		# 1 Non-recursive
+    .text
+    .globl main
 
 main:
     # read the integer n from the standard input
-	jal readInt
-	# now $v0 contains the number of disk n
+    jal readInt
+
+    # now $v0 contains the number of disk n
+
     # check for which algorithm is set to use: Recursive or non-recursive.
     la $t0 AlgorithmType
-	lw $t0 ($t0)
-	beq $t0 0 TOH_Recursive
-	beq $t0 1 TOH_Nonrecursive
+    lw $t0, 0($t0)
+
+    # Initialize arguments to TOH
+    move $a0, $v0
+    li $a1, 'A'
+    li $a2, 'C'
+    li $a3, 'B'
+
+    # Use $s0 as the step counter.
+    li $s0, 1
+
+    beq $t0 0 TOH_Recursive
+    beq $t0 1 TOH_Nonrecursive
 
     # exit if the algorithm number is out of range
-	li $v0 10 
-    syscall	
+    li $v0 10 
+    syscall
+
 
 TOH_Recursive:
-# TODO:
-#       Use a recursive algorithm described in the assignment document to print the solution steps. Make sure you follow the output format.
-# Set the first breakpoint to measure cache performance and instruction count for the recursive method at the first instruction of this label
+    jal hanoi_rec
 
+    li $v0 10
+    syscall
+
+hanoi_rec:
+    # Inputs:
+    #    $a0: n
+    #    $a1: source
+    #    $a2: target
+    #    $a3: auxiliary
+    addi $sp, $sp, -20
+    sw $ra, 0($sp)
+    sw $a0, 4($sp)
+    sw $a1, 8($sp)
+    sw $a2, 12($sp)
+    sw $a3, 16($sp)
+
+    # Branch if not base case
+    li $t0, 1
+    bne $a0, $t0, delegate
+
+    # base case: n == 1
+
+    # Here we actually move disk from source to target
+    jal print_move
+
+    lw $ra, 0($sp)
+    lw $a0, 4($sp)
+    lw $a1, 8($sp)
+    lw $a2, 12($sp)
+    lw $a3, 16($sp)
+    addi $sp, $sp, 20
+
+    jr $ra
+
+
+
+
+delegate:
+    subi $a0, $a0, 1
+
+    # Temp to store target
+    move $t0, $a2
+
+    # target becomes auxiliary
+    move $a2, $a3
+
+    # auxiliary becomes target
+    move $a3, $t0
+
+    # MOVE(n-1, source, aux, target)
+    jal TOH_Recursive
+
+    # Actually move disk
+    jal print_move
+
+    # Temp to store target
+    move $t0, $a3
+
+    # third argument becomes source
+    move $a3, $a1
+
+    # first argument becomes aux
+    move $a1, $a2
+
+    # second argument becomes target
+    move $a2, $t0
+
+    # MOVE(n-1, aux, target, source)
+    jal TOH_Recursive
 
 
 
@@ -58,6 +141,59 @@ syscall
 
 # TODO: your functions here
 
+print_move: 
+    # Input:
+    #    $a0: n
+    #    $a1: source
+    #    $a2: target
+    # Uses:
+    #    $s0: counter
+    # Print "Step {counter}: move disk {n} from {source} to {target}"
+    
+    # Save $ra on stack
+    addi $sp, $sp, -8
+    sw $ra, 0($sp)
+    # Store $a0 as we will be overwriting it in calls to subroutines
+    sw $a0, 4($sp)
+
+    # Store n somewhere else since we still do need to print it
+    move $t0, $a0
+
+    # Print "Step "
+    la $a0, Step
+    jal printStr
+
+    # Print counter
+    move $a0, $s0
+    jal printInt
+
+    # Print ": move disk "
+    la $a0, Colon
+    jal printStr
+    la $a0, Move
+    jal printStr
+
+    # Print disk number
+    move $a0, $t0
+    jal printInt
+
+    # Print " from {source} to {target}\n"
+    la $a0, From
+    jal printStr
+    move $a0, $a1
+    jal printChar
+    la $a0, To
+    jal printStr
+    move $a0, $a2
+    jal printChar
+    la $a0, Newline
+    jal printStr
+
+    # Restore $ra and return
+    lw $ra, 0($sp)
+    lw $a0, 4($sp)
+    addi $sp, $sp, 8
+    jr $ra
 
 
 
--- COMP273/hanoifs.asm
+++ COMP273/hanoifs.asm
@@ -1,10 +1,4 @@
     .data
-Alice:   .asciiz "Alice "
-Bob:     .asciiz "Bob "
-Charlie: .asciiz "Charlie "
-Moved:   .asciiz "moved disk "
-From:    .asciiz " from "
-To:      .asciiz " to "
 
 # Save a buffer to concatenate the above strings into to print the move
 move_buffer .space 100
@@ -18,41 +12,6 @@ move_buffer .space 100
     # Second delegating task is always Bob's
     # Base case task is always Alice's
 
-main:
-    jal hanoi_rec
-
-
-    # Hanoi function has inputs:
-    # n: number of disks on the tower
-    # source: Peg on which all disks start
-    # target: Desired location to move all disks
-    # auxiliary: Some third peg
-hanoi_rec:
-    # Always save 4 arguments and $ra
-    addi $sp, $sp, -20
-    sw $ra, 0($sp)
-    sw $a0, 4($sp)    # $a0 == n
-    sw $a1, 8($sp)    # $a1 == source
-    sw $a2, 12($sp)   # $a2 == target
-    sw $a3, 16($sp)   # $a3 == aux
-
-    li $t0, 1
-    bne $a0, $t0, delegate
-
-    # Here we actually move disk from source to target
-    # Save aux and use $a3 to store Alice's name (her turn to move disk) 
-    addi $sp, $sp, -8
-    sw $ra, 0($sp)
-    sw $a3, 4($sp)
-    la $a3, Alice
-    jal concat_string
-
-
-
-
-delegate:
-    subi $a0, $a0, 1
-    jal hanoi_rec
 
 
 print_move:
